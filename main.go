9add8675b81126813a25cd99c02c4d95fd13ebf1
Mike Mulderink <mulderink.code@gmail.com>
2020-05-01 15:41:22 -0500

    Added File Path comments in main.go

--- main.go
+++ main.go
@@ -9,15 +9,19 @@ func main() {
 }
 
 func upTripRoute() {
+	//  File Path must be exact when running the executable
 	http.Handle("/melissa-mike/assets/", http.StripPrefix("/melissa-mike/assets", http.FileServer(http.Dir("/GoLang/src/github.com/mikes_personal_site_golang/Up-Trip/assets"))))
 	http.HandleFunc("/melissa-mike/", func(w http.ResponseWriter, r *http.Request) {
+		//  File Path must be exact when running the executable
 		http.ServeFile(w, r, "/GoLang/src/github.com/mikes_personal_site_golang/Up-Trip/index.html")
 	})
 }
 
 func homeRoute() {
+	//  File Path must be exact when running the executable
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("/GoLang/src/github.com/mikes_personal_site_golang/Home/assets"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//  File Path must be exact when running the executable
 		http.ServeFile(w, r, "/GoLang/src/github.com/mikes_personal_site_golang/Home/index.html")
 	})
 }
