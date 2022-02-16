96b5848a4cf1a7537b54cec768229c47b1249523
Mike Mulderink <mulderink1@gmail.com>
2022-02-15 17:46:44 -0800

    Updated HTML and filepaths in main.go based on OS

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"net/http"
+	"runtime"
 )
 
 func main() {
 	homeRoute()
-
 	http.ListenAndServe(":8080", nil)
 }
 
 func homeRoute() {
-	http.Handle("/", http.FileServer(http.Dir("/home/ubuntu/web/static")))
+	os := runtime.GOOS
+	switch os {
+	case "linux":
+		http.Handle("/", http.FileServer(http.Dir("/home/ubuntu/web/static")))
+	default:
+		http.Handle("/", http.FileServer(http.Dir("/Users/michaelmulderink/go/src/github.com/Mike_Personal_Site/web/static")))
+	}
 }
