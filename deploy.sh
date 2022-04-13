398061854aca9fe8b0d886339f6f2b9ea1dbdf05
Mike Mulderink <mulderink1@gmail.com>
2022-04-13 08:38:51 -0700

    Added to HTML

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,7 @@ GOOS=linux GOARCH=amd64 go build main.go
 mv main web/main
 
 scp -i /Users/michaelmulderink/mikes_personal_site/Mikes_Site.pem -r ../Mike_Personal_Site/web/ ubuntu@ec2-52-200-134-54.compute-1.amazonaws.com:
+
+cd web
+
+rm main
